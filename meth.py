ef1fc4433ae8b7d842a9d3d83f2e8b11a8415be4
zhisaoka <zhisaoka@gmail.com>
2025-05-09 09:36:35 -0700

    Create meth.py

---
+++ meth.py
@@ -0,0 +1,12 @@
+from math import *
+
+def percent(x):
+    return(x/100)
+
+def checkNeg(x):
+    if x <= 0:
+        print("Negetive detected:", x)
+        return True
+
+def inverse(x):
+    return(x*-1)
